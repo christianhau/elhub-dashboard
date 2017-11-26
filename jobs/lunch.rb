895a54154128a0f6b6c621fab94a3a33ca792e0c
Kristoffer Lorentsen <krilor@gmail.com>
2017-11-26 18:24:29 +0100

    Adds dagens lunch istead of HELLO

---
+++ jobs/lunch.rb
@@ -0,0 +1,17 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
+SCHEDULER.every '1h', :first_in => 0 do |job|
+    uri = URI("https://sulten.81549300.xyz")
+    response = Net::HTTP.get(uri)
+    today = JSON.parse(response)["idag"]
+
+    unless today.kind_of?(Array)
+      today = [today]
+    end
+
+    dishes = today.join('\n')
+
+    send_event('lunch', { text: dishes })
+end
